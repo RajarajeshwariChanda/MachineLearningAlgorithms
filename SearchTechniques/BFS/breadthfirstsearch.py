ff5197678bc6844d0eebfb91da0a9fb3ccae2dac
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2025-04-30 14:25:10 +0530

    created the graph adjacency list represnetation

--- SearchTechniques/BFS/breadthfirstsearch.py
+++ SearchTechniques/BFS/breadthfirstsearch.py
@@ -1,8 +1,25 @@
 import collections # from collection dequeue
+graph={
+    "S": ["A","B","C"],
+    "A": ["D"],
+    "B": ["E","S"],
+    "c": ["S","F","J"],
+    "D": ["A","G","H"],
+    "E": ["B","I","J"],
+    "F": ["S","C"],
+    "G": ["H","D"],
+    "H": ["D","G"],
+    "I": ["E"],
+    "J": ["E"],
+}
+
 def main():
     print("entering the main function")
+    outcome=bfs()
 def bfs():
     print("entering the bfs function")
 
 if __name__ == "__main__":
     main()
+
+
