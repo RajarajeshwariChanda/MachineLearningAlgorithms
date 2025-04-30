b5af8d869e43c164db0908ef6a2f48d65d5cc289
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2025-04-30 14:20:34 +0530

    Create breadthfirstsearch.py
    creating the function and initialing the main function

---
+++ SearchTechniques/BFS/breadthfirstsearch.py
@@ -0,0 +1,8 @@
+import collections # from collection dequeue
+def main():
+    print("entering the main function")
+def bfs():
+    print("entering the bfs function")
+
+if __name__ == "__main__":
+    main()
